389dba3db6f51a901b025f2161f6196ca67fb494
Paulson Robert <paulsonr07@gmail.com>
2024-06-02 20:15:31 +0530

    Initial commit for tailor app

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "../styles/globals.css";
+import { ReactNode } from "react";
+import Head from "next/head";
+
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>STITCHIT-Tailor</title>
+      </Head>
+      <body className="bg-gray-100 text-gray-900">{children}</body>
+    </html>
+  );
+}
